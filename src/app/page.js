252d9fe49cf08152e8d32e3bb7de4ef5cffc8774
Daniela <dare0002@stud.kea.dk>
2024-11-26 14:03:35 +0100

    btn and h1 removed from mobile

--- src/app/page.js
+++ src/app/page.js
@@ -8,15 +8,15 @@ export default function Home() {
     <div className="relative w-full h-screen">
       <Image src={heroImage} alt="Image with different products" width={1440} height={697}  layout="intrinsic" objectFit="cover"  />
       <div className="absolute inset-0 flex flex-col justify-center items-center text-black">
-        <h1 className="text 6xl md:text-6xl font-bold text-center mb-4">REWU</h1>
-        <button className="bg-green text-saddle50 py-2 px-6 rounded-lg text-lg hover:bg-saddle100">Products</button>
+        <h1 className="text 6xl md:text-6xl font-bold text-center mb-4 hidden md:block">REWU</h1>
+        <button className="bg-green text-saddle50 py-2 px-6 rounded-lg text-lg hover:bg-saddle100 hidden md:block">Products</button>
       </div>
     </div>
-    <div className="bg-saddle50 flex flex-col items-center py-12">
+    {/* <div className="bg-saddle50 flex flex-col items-center py-12">
       <h3 className="text-saddle900">Check out our bestsellers</h3>
       <Image src={mockupImage} alt="Image with make-up product, nail polish, perfume and chili" width={200} height={200} objectFit="contain"/>
 
-    </div>
+    </div> */}
     
   </section>
   );
